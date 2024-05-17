4eae64875e39478bcaf218c994e79add542d7068
Arjab Khadka <arjabkhadka93@gmail.com>
2024-05-17 07:57:07 +0545

    update: AddExpenseModal added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import BudgetCard from "./components/BudgetCard";
 import AddBudgetModal from "./components/AddBudgetModal";
+import AddExpenseModal from "./components/AddExpenseModal";
 import { useState } from "react";
 import { useBudgets } from "./contexts/BudgetsContext"
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+  const [addExpenseModalBudgetId, setaddExpenseModalBudgetId] = useState()
   const { budgets, getBudgetExpenses } = useBudgets()
+
+  function openAddExpenseModal(budgetId) {
+    setShowAddBudgetModal(true)
+    setaddExpenseModalBudgetId(budgetId)
+  }
+
   return (
     <>
       < Container className="my-4">
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto">Budgets</h1>
           <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
-          <Button variant="outline-primary">Add Expense</Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
         </Stack>
         <div
           style={{
@@ -35,6 +44,7 @@ function App() {
                 name={budget.name}
                 amount={amount}
                 max={budget.max}
+                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
               />
             )
           })}
@@ -42,6 +52,10 @@ function App() {
       </Container >
       <AddBudgetModal show={showAddBudgetModal} handleClose={() =>
         setShowAddBudgetModal(false)} />
+      <AddExpenseModal show={showAddExpenseModal}
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() =>
+          setShowAddExpenseModal(false)} />
     </>
   )
 }
